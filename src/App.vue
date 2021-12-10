08af016b787aedb00acf8558a95ac020211afe9d
Alexander <alex.budnik.dev@gmail.com>
2021-12-10 14:28:26 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+
+<template>
+  <!-- Application template -->
+  <v-app>
+      <div class="players-area">
+          <h2>Tournament players</h2>
+         <v-text-field class="players-cnt-input" label="Players count"/>
+          <v-btn color="primary" elevation="2">Generate players</v-btn>
+         <PlayerList v-bind:players="players"/>
+      </div>
+  </v-app>
+</template>
+
+<script>
+
+/**Import component */
+import PlayerList from '@/components/PlayerList'
+
+export default {
+  name: 'App',
+
+  /**Registration components */
+  components: {
+    PlayerList
+  },
+
+  /**Data declare */
+  data() { 
+   return { 
+          players:[ 
+            {id: 1, full_name: 'Jimi Hendrix',    description: 'Мusician',      color:"#2c3e50"},
+            {id: 2, full_name: 'Bill Gates',      description: 'Ex. Dirrector', color:"#2c3e50" },
+            {id: 3, full_name: 'Linus Torvalds',  description: 'Real coder',    color:"#2c3e50" },
+            {id: 4, full_name: 'Dave Mastane',    description: 'Мusician',      color:"#2c3e50" },
+            {id: 5, full_name: 'Madame Curie',    description: 'Scientist',     color:"#2c3e50" }
+          ]
+        }
+    },
+  };
+
+</script>
+
+
+
+<style>
+
+/** application style*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+
+.players-area { 
+    padding: 22px;
+    height: 100%; 
+    width: auto; 
+    position: fixed;
+    min-width: 300px;
+    z-index: 1; 
+    background-color: rgb(244, 244, 244); 
+    overflow-x: hidden; 
+    box-shadow: 2px 0 25px -2px #888;
+    text-align: left;
+  }
+
+
+</style>
---
+++ src/components/PlayerList.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <!-- table template -->
+
+     <v-list two-line>
+            <v-list-item-group v-model="selectedItem" color="primary" >
+                  <v-list-item v-for="player in players" :key="player.id" >
+              
+                    <v-list-item-avatar>
+                      <v-avatar color="green" size="62">
+                      <span class="white--text text-h5">{{player.full_name[0]}}</span>
+                      </v-avatar>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title v-text="player.full_name"></v-list-item-title>
+                      <v-list-item-subtitle v-text="player.description"></v-list-item-subtitle>
+                    </v-list-item-content>
+
+                  </v-list-item>
+                </v-list-item-group>
+           </v-list>
+</template>
+ 
+<script>
+/** Export as default obj*/
+  export default {
+    props: ['players'] 
+  }
+</script>
+
+
+<style scoped>
+/** Private style*/
+
+  .theme--light {
+      background: rgb(244, 244, 244); ;
+      color: rgba(0, 0, 0, 0.87);
+  }
+  /**TODO: Fix white spases*/
+  .v-item-group {
+    margin-top: -8px;
+    margin-bottom: -8px;
+  }
+  .v-list {
+    margin-top: 8px;
+  }
+
+  .white--text {
+    margin-left: -22px;
+  }
+</style>
